4b78b9d95b62e66dd58ffaffaefcfd1775b37319
sooheon <tngjs0@gmail.com>
2016-05-14 02:32:12 +0900

    Inital commit w/ basic functionality
    Korean number unit parsing and automatic conversion.

---
+++ src/cljs/cwonversion/app.cljs
@@ -0,0 +1,78 @@
+(ns cwonversion.app
+  (:require [reagent.core :as reagent :refer [atom]]
+            ;; [ajax.core :refer [GET]]
+            [cljs.pprint :as pp]
+            [clojure.string :as str]))
+
+(def kr-number-units
+  {"원" 1
+   "일" 1
+   "십" 10
+   "백" 100
+   "천" 1000
+   "만" 1e4
+   "억" 1e8
+   "조" 1e12})
+
+(def us-number-units
+  {"hundred" 100
+   "thousand" 1e3
+   "million" 1e6
+   "billion" 1e9
+   "trillion" 1e12})
+
+(def app-state (atom {:usd->krw 1175.25
+                      :krw->usd (/ 1 1175.25)
+                      :krw nil
+                      :usd nil}))
+
+(defn forex-display []
+  [:p "The current FOREX rate for USD and KRW is: "
+   [:b (:usd->krw @app-state)]])
+
+(defn parse-number
+  "Takes a Korean number string, such as '100만', and parses it as an integer."
+  [country s]
+  (let [digits (re-find #"\d*[\.\d+]?\d*" s)
+        n (if (seq digits) digits 1)
+        units (re-find #"\D+" s)
+        parser (case country
+                 :krw kr-number-units
+                 :usd us-number-units)]
+    (if (empty? s)
+      0
+      (apply * n (map parser units)))))
+
+(defn convert [input-str from to]
+  (let [xr (case from
+             :krw (:krw->usd @app-state)
+             :usd (:usd->krw @app-state))]
+    (swap! app-state assoc from input-str)
+    (swap! app-state assoc to (.toLocaleString
+                               (* xr (parse-number from input-str))))))
+
+(defn currency-input [from to]
+  [:form {:on-submit #(.preventDefault %)}
+   [:input {:type "text"
+            :value (from @app-state)
+            :on-change #(convert (-> % .-target .-value)
+                                 from to)}]
+   (str/upper-case (str " " (name from)))])
+
+(defn converter-container []
+  [:div
+   [currency-input :krw :usd]
+   [currency-input :usd :krw]])
+
+(defn app-state-display []
+  [:pre (with-out-str (pp/pprint @app-state))])
+
+(defn home []
+  [:div
+   [forex-display]
+   [converter-container]
+   [app-state-display]])
+
+(defn init []
+  (reagent/render [home]
+                  (.getElementById js/document "container")))
---
+++ test/cljs/cwonversion/app_test.cljs
@@ -0,0 +1,26 @@
+(ns cwonversion.app-test
+  (:require-macros [cljs.test :refer [deftest testing is]])
+  (:require [cljs.test :as t]
+            [cwonversion.app :as cw]))
+
+(deftest test-kr-number-parse []
+  (is (= (cw/parse-number :krw "만원")
+         1e4))
+  (is (= (cw/parse-number :krw "십조원")
+         (* 10 1e12)))
+  (is (= (cw/parse-number :krw "100원")
+         100))
+  (is (= (cw/parse-number :krw "100만원")
+         (* 100 1e4)))
+  (is (= (cw/parse-number :krw "0.1만원")
+         (* 0.1 1e4)))
+  (is (= (cw/parse-number :krw "1원")
+         1)))
+
+(deftest test-us-num-parse []
+  (is (= (cw/parse-number :usd "1 thousand")
+         1000)))
+
+(deftest re-test []
+  (is (re-find #"\d*[\.\d+]?\d*" "1.4 thousand")
+      "1.4"))
